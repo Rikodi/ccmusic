5fd7d0b4c0beb3224f1bc436409dbab3f550a33f
Rikodi <38777376+Rikodi@users.noreply.github.com>
2022-12-14 13:45:33 -0700

    Add files via upload

---
+++ jams.lua
@@ -0,0 +1,31 @@
+function bonk()
+	shell.run("art.lua liaBonk")
+	shell.run("clear")
+	print("Flip the switch to JAM!")
+end
+
+function waitForRS()
+	repeat uuntil os.pullEvent("redstone")
+end
+
+function jam()
+	sleep(0)
+	if rs.getInput("bottom") then
+		shell.run("art.lua liaRave")
+		shell.run('speaker play "Polyphia - The Worst.dfpwm"')
+	else
+		shell.run("speaker stop")
+		bonk()
+	end
+end
+
+function stop()
+	waitForRS()
+end
+
+shell.run("monitor left clear")
+bonk()
+waitForRS()
+while true do
+	parallel.waitForAny(jam, stop)
+end
